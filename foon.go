f52a323a26fff6ba2b2c6c824a0f42ebc489caef
zakuni <kunio038@gmail.com>
2014-07-23 00:23:21 +0900

    add a concept of pages

--- foon.go
+++ foon.go
@@ -6,12 +6,25 @@ import (
 	"os"
 )
 
+type Page struct {
+  Title string
+  Body string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
   t, _ :=  template.ParseFiles("index.html")
   t.Execute(w, nil)
 }
 
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("page.html")
+  title := r.URL.Path[len("/pages/"):]
+  p := &Page{Title: title, Body: ""}
+  t.Execute(w, p)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+  http.HandleFunc("/pages/", pageHandler)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
