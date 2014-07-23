65973cf26841efac8ecd5fc8b4051c134b6c18f0
zakuni <kunio038@gmail.com>
2014-07-24 00:28:05 +0900

    abolish /pages/

--- foon.go
+++ foon.go
@@ -4,6 +4,7 @@ import (
   "html/template"
 	"net/http"
 	"os"
+  "regexp"
 )
 
 type Page struct {
@@ -12,19 +13,25 @@ type Page struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  t, _ :=  template.ParseFiles("index.html")
-  t.Execute(w, nil)
-}
-
-func pageHandler(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("page.html")
-  title := r.URL.Path[len("/pages/"):]
-  p := &Page{Title: title, Body: ""}
-  t.Execute(w, p)
+  re := regexp.MustCompile("^/([^/]*)/?([^/]*)$")
+  params := re.FindStringSubmatch(r.URL.Path)
+  wiki := params[1]
+  page := params[2]
+  if params[1] == "" {
+    t, _ :=  template.ParseFiles("index.html")
+    t.Execute(w, nil)
+  } else if params[2] == "" {
+    p := &Page{Title: wiki, Body: ""}
+    t, _ := template.ParseFiles("page.html")
+    t.Execute(w, p)
+  } else {
+    p := &Page{Title: page, Body: ""}
+    t, _ := template.ParseFiles("page.html")
+    t.Execute(w, p)
+  }
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-  http.HandleFunc("/pages/", pageHandler)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
