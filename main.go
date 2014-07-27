893dd06d69ace5266d590070ca49040c4e4fa96c
zakuni <kunio038@gmail.com>
2014-07-27 14:07:19 +0900

    rename foon.go => main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"foon/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,6 +11,8 @@ import (
 
 	"github.com/coopernurse/gorp"
 	_ "github.com/lib/pq"
+
+	"foon/models"
 )
 
 var dbmap *gorp.DbMap
