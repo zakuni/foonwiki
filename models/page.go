b4e9a3ffc24ef31068225d6b89b4e33f38341a69
zakuni <kunio038@gmail.com>
2014-08-06 22:15:27 +0900

    add CreatedAt and UpdatedAt to models

--- models/page.go
+++ models/page.go
@@ -1,9 +1,15 @@
 package models
 
+import (
+	"time"
+)
+
 // Page is the main content of wiki
 type Page struct {
 	Id      int64
 	Name    string
 	Content string
 	WikiId  int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- models/wiki.go
+++ models/wiki.go
@@ -1,9 +1,15 @@
 package models
 
+import (
+	"time"
+)
+
 // Wiki has many pages
 type Wiki struct {
 	Id   int64
 	Name string `sql:"unique"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 	Pages []Page
 }
