91fca5c6979ad26256018befc7e389168cf29400
zakuni <kunio038@gmail.com>
2016-07-17 19:54:16 +0900

    use power-assert to assertion

--- test/components/test_pageform.js
+++ test/components/test_pageform.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert, expect } from 'chai';
+import assert from 'power-assert';
 
 import PageForm from '../../components/pageform.jsx';
 
 describe('<PageForm>', () => {
   it('should render one <form>', () => {
     const wrapper = shallow(<PageForm />);
-    expect(wrapper.find('form')).to.have.length(1);
+    assert.equal(wrapper.find('form').length, 1);
   });
 });
