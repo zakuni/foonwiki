0fd9fbe272b909070f6214459d2605d4f6fa68dc
zakuni <kunio038@gmail.com>
2016-08-17 23:17:16 +0900

    add test for page component

---
+++ test/components/test_pageapp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import assert from 'power-assert';
+
+import PageApp from '../../components/page';
+import PageTitle from '../../components/pagetitle.jsx';
+import PageContent from '../../components/pagecontent.jsx';
+
+describe('<PageApp>', () => {
+  it('has correct nodes', () => {
+    const wrapper = shallow(<PageApp />);
+    assert.equal(wrapper.childAt(0).type(), 'div');
+    assert.equal(wrapper.find(PageTitle).length, 1);
+    assert.equal(wrapper.find(PageContent).length, 1);
+  });
+});
