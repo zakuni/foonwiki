d44c48c424cebd8936a1dda6a6df7c8047fea8de
zakuni <kunio038@gmail.com>
2016-09-23 20:35:47 +0900

    delete unnecessary style with PageContentRow

--- components/pagecontentrow.jsx
+++ components/pagecontentrow.jsx
@@ -2,15 +2,10 @@ import React, { Component, PropTypes } from 'react';
 
 class PageContentRow extends Component {
   render() {
-    var style = {
-      whiteSpace: 'pre',
-      marginBottom: '20px'
-    };
     return (
       <div
           className="editable cursor-text"
           contentEditable="true"
-          style={style}
           ref={node => this.editable = node}
           onInput={() => this.props.onContentChange(this.editable.innerHTML)}
       >
