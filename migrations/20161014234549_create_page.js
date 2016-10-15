364f05745f15713e4be9124ed2112d4e6dc03eb4
zakuni <kunio038@gmail.com>
2016-10-15 14:57:27 +0900

    add column to migration file

--- migrations/20161014234549_create_page.js
+++ migrations/20161014234549_create_page.js
@@ -3,6 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('pages', function (table) {
     table.increments('id').primary();
     table.timestamps();
+    table.text('name');
+    table.text('content');
   });
 };
 
