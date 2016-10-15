8a5c6b7ce1ebd6f49de1ac16493b253221d71f0f
zakuni <kunio038@gmail.com>
2016-10-15 10:14:04 +0900

    modify create_page migration

--- migrations/20161014234549_create_page.js
+++ migrations/20161014234549_create_page.js
@@ -1,6 +1,9 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('pages', function () {});
+  return knex.schema.createTableIfNotExists('pages', function (table) {
+    table.increments('id').primary();
+    table.timestamps();
+  });
 };
 
 exports.down = function(knex, Promise) {
