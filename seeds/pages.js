86d9ed26020a77143255330429ae7eec9f62ffe2
zakuni <kunio038@gmail.com>
2016-10-15 16:50:44 +0900

    fix seed file
    insert data after table is deleted

--- seeds/pages.js
+++ seeds/pages.js
@@ -2,10 +2,13 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('pages').del(),
-
-    // Inserts seed entries
-    knex('pages').insert({id: 1, name: 'name', content: 'content'}),
-    knex('pages').insert({id: 2, name: 'name2', content: 'content2'})
+    knex('pages').del()
+    .then(function() {
+      return Promise.all([
+        // Inserts seed entries
+        knex('pages').insert({id: 1, name: 'name', content: 'content'}),
+        knex('pages').insert({id: 2, name: 'name2', content: 'content2'})
+      ]);
+    })
   );
 };
