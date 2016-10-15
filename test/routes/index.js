3e49b1f8ccbbef22f0be899d9b172419f88677e3
zakuni <kunio038@gmail.com>
2016-10-15 14:51:25 +0900

    add test with id query

--- test/routes/index.js
+++ test/routes/index.js
@@ -2,9 +2,31 @@ import request from 'supertest';
 import app from '../../app';
 
 describe('GET /', () => {
-  it('respond with status 200', (done) => {
-    request(app)
-      .get('/')
-      .expect(200, done);
+  context('without query', () => {
+    it('respond with status 200', (done) => {
+      request(app)
+        .get('/')
+        .expect(200, done);
+    });
+  });
+
+  context('with valid query', () => {
+    context('with existing id', () => {
+      it('respond with status 200', (done) => {
+        request(app)
+          .get('/')
+          .query('id=1')
+          .expect(200, done);
+      });
+    });
+  });
+
+  context('with invalid query', () => {
+    it('respond with status 200', (done) => {
+      request(app)
+        .get('/')
+        .query('invalid')
+        .expect(200, done);
+    });
   });
 });
